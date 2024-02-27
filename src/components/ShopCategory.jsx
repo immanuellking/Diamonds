53dac0194f15fcc01338c8fa950935ec81cfcd78
immanuellking <emmaebubeegbosi@gmail.com>
2024-02-27 18:31:16 +0100

    shop category done and responsive

---
+++ src/components/ShopCategory.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ShopCategory = () => {
+  return (
+    <section>
+      <h1 className="text-center text-2xl sm:text-3xl lg:text-4xl">
+        Shop By Category
+      </h1>
+    </section>
+  );
+};
+
+export default ShopCategory;
