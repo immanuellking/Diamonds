bca71e73c874d42d639598cef55647457773ec76
immanuellking <emmaebubeegbosi@gmail.com>
2024-02-27 12:46:31 +0100

    added shop by chape & category components to hompage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import ShopByShape from "../components/ShopByShape";
+import ShopCategory from "../components/ShopCategory";
 
 const HomePage = () => {
   return (
     <div>
       <Header />
       <Hero />
+      <ShopByShape />
+      <ShopCategory />
     </div>
   );
 };
