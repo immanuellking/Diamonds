dd52609c8229cdaaa65e0e956a8aea7ac6d41086
immanuellking <emmaebubeegbosi@gmail.com>
2024-02-27 23:50:35 +0100

    bridal edit section now responsive

--- src/components/BridalEdit.jsx
+++ src/components/BridalEdit.jsx
@@ -9,40 +9,40 @@ const BridalEdit = () => {
       </div>
 
       <main className="w-full grid grid-cols-12 gap-x-8 gap-y-10">
-        <div className="w-full col-span-6">
+        <div className="w-full col-span-6 space-y-4 group ">
             <div>
                 <img src="/bridal/nature.avif" className="w-full" alt="" />
             </div>
-            <div>
-                <h4 className="text-3xl">Nature Inspired Rings</h4>
-                <p>Shop Now</p>
+            <div className="group-hover:text-[#0d6363]">
+                <h4 className="text-base sm:text-xl lg:text-3xl">Nature Inspired Rings</h4>
+                <p className="underline text-sm sm:text-base">Shop Now</p>
             </div>
         </div>
-        <div className="w-full col-span-6">
+        <div className="w-full col-span-6 space-y-4 group ">
             <div>
                 <img src="/bridal/Three.avif" className="w-full" alt="" />
             </div>
-            <div>
-                <h4>Three Stone Rings</h4>
-                <p>Shop Now</p>
+            <div className="group-hover:text-[#0d6363]">
+                <h4 className="text-base sm:text-xl lg:text-3xl">Three Stone Rings</h4>
+                <p className="underline text-sm sm:text-base">Shop Now</p> 
             </div>
         </div>
-        <div className="w-full col-span-6">
+        <div className="w-full col-span-6 space-y-4 group ">
             <div>
                 <img src="/bridal/bridal.avif" className="w-full" alt="" />
             </div>
-            <div>
-                <h4>Bridal Sets</h4>
-                <p>Shop Now</p>
+            <div className="group-hover:text-[#0d6363]">
+                <h4 className="text-base sm:text-xl lg:text-3xl">Bridal Sets</h4>
+                <p className="underline text-sm sm:text-base">Shop Now</p>
             </div>
         </div>
-        <div className="w-full col-span-6">
+        <div className="w-full col-span-6 space-y-4 group ">
             <div>
                 <img src="/bridal/hidden.avif" className="w-full" alt="" />
             </div>
-            <div>
-                <h4>Hidden Halo Rings</h4>
-                <p>Shop Now</p>
+            <div className="group-hover:text-[#0d6363]">
+                <h4 className="text-base sm:text-xl lg:text-3xl">Hidden Halo Rings</h4>
+                <p className="underline text-sm sm:text-base">Shop Now</p>
             </div>
         </div>
       </main>
