4abf6784ba8cf74f74a2c41b7244200620e4fae9
immanuellking <emmaebubeegbosi@gmail.com>
2024-02-29 18:49:08 +0100

    changed image src to image address in jewelry section

--- src/components/Jewelry.jsx
+++ src/components/Jewelry.jsx
@@ -9,7 +9,11 @@ const Jewelry = () => {
         <div className="flex gap-x-4 lg:gap-x-8 min-w-[1000px] sm:min-w-full">
           <div className="w-full space-y-4 group ">
             <div>
-              <img src="/personalized.avif" className="w-full" alt="" />
+              <img
+                src="https://image.brilliantearth.com/cdn-cgi/image/width=443,height=513,quality=100,format=auto/https://cdn.builder.io/api/v1/image/assets%2F9f2a69003c86470ea05deb9ecb9887be%2Fcaa9166fa9e5495db554c3734dc0071c"
+                className="w-full"
+                alt=""
+              />
             </div>
             <h4 className="text-base lg:text-lg group-hover:text-[#0d6363]">
               Personalized Jewerly
@@ -17,7 +21,11 @@ const Jewelry = () => {
           </div>
           <div className="w-full space-y-4 group ">
             <div>
-              <img src="/heart-shaped.avif" className="w-full" alt="" />
+              <img
+                src="https://image.brilliantearth.com/cdn-cgi/image/width=443,height=513,quality=100,format=auto/https://cdn.builder.io/api/v1/image/assets%2F9f2a69003c86470ea05deb9ecb9887be%2F7a3a092fa90d495e800b82f72ea4b7bf"
+                className="w-full"
+                alt=""
+              />
             </div>
             <h4 className="text-base lg:text-lg group-hover:text-[#0d6363]">
               Heart Shaped Jewelry
@@ -25,7 +33,11 @@ const Jewelry = () => {
           </div>
           <div className="w-full space-y-4 group ">
             <div>
-              <img src="/tennis.avif" className="w-full" alt="" />
+              <img
+                src="https://image.brilliantearth.com/cdn-cgi/image/width=443,height=513,quality=100,format=auto/https://cdn.builder.io/api/v1/image/assets%2F9f2a69003c86470ea05deb9ecb9887be%2F0196dc2883be464ba1e7794eacbed1a7"
+                className="w-full"
+                alt=""
+              />
             </div>
             <h4 className="text-base lg:text-lg group-hover:text-[#0d6363]">
               Tennis Jewelry
@@ -33,7 +45,11 @@ const Jewelry = () => {
           </div>
           <div className="w-full space-y-4 group ">
             <div>
-              <img src="/gold.avif" className="w-full" alt="" />
+              <img
+                src="https://image.brilliantearth.com/cdn-cgi/image/width=443,height=513,quality=100,format=auto/https://cdn.builder.io/api/v1/image/assets%2F9f2a69003c86470ea05deb9ecb9887be%2F75587660789c4a96a3921fa7f30c21ed"
+                className="w-full"
+                alt=""
+              />
             </div>
             <h4 className="text-base lg:text-lg group-hover:text-[#0d6363]">
               Gold Chains
