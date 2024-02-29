6d0fe267a88956f53da7be4ba8e3ecc1fd890af2
immanuellking <emmaebubeegbosi@gmail.com>
2024-03-01 00:41:12 +0100

    increase hero image width and added margin top

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <section className="w-full h-[85vh] lg:h-[75vh] px-12 flex justify-end sm:justify-center lg:justify-end items-end lg:items-center bg-center bg-cover bg-no-repeat sm:bg-cover bg-[url('https://image.brilliantearth.com/cdn-cgi/image/width=998,height=1330,quality=100,format=auto/https://cdn.builder.io/api/v1/image/assets%2F9f2a69003c86470ea05deb9ecb9887be%2F71d996c369994ce999ca8b4e5358e85a')] sm:bg-[url('https://image.brilliantearth.com/cdn-cgi/image/width=2880,height=1040,quality=100,format=auto/https://cdn.builder.io/api/v1/image/assets%2F9f2a69003c86470ea05deb9ecb9887be%2Fee11053e12eb482cbead685201881210')]">
+    <section className="w-full h-[95vh] mt-10 sm:mt-0 lg:h-[75vh] px-12 flex justify-end sm:justify-center lg:justify-end items-end lg:items-center bg-center bg-cover bg-no-repeat sm:bg-cover bg-[url('https://image.brilliantearth.com/cdn-cgi/image/width=998,height=1330,quality=100,format=auto/https://cdn.builder.io/api/v1/image/assets%2F9f2a69003c86470ea05deb9ecb9887be%2F71d996c369994ce999ca8b4e5358e85a')] sm:bg-[url('https://image.brilliantearth.com/cdn-cgi/image/width=2880,height=1040,quality=100,format=auto/https://cdn.builder.io/api/v1/image/assets%2F9f2a69003c86470ea05deb9ecb9887be%2Fee11053e12eb482cbead685201881210')]">
       <div className="space-y-6 mb-8 lg:mb-0">
         <h1 className="text-center lg:text-left text-4xl">Coming Up Roses</h1>
         <p className="text-center lg:text-left">
