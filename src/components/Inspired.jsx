281df758f1ba8e59b3bfbb62c05912f2892c5fea
immanuellking <emmaebubeegbosi@gmail.com>
2024-02-29 08:19:14 +0100

    inpired section done an dresponsive, added css style to hide the scroll bar

--- src/components/Inspired.jsx
+++ src/components/Inspired.jsx
@@ -6,33 +6,42 @@ const Inspired = () => {
 
   return (
     <section className="px-4 lg:px-8 py-8">
-      <main className="bg-[#183E40] py-12 space-y-12">
-        <div className="text-white px-8 flex justify-between items-end">
-          <h1 className="text-4xl tracking-wider">Get Inspired</h1>
-          <p className="underline">Shop Insta</p>
+      <main className="bg-[#183E40] pt-8 pb-10 sm:pb-16  lg:pt-12 lg:pb-12 space-y-8 lg:space-y-12">
+        <div className="text-white px-4 lg:px-8 flex justify-center sm:justify-between items-end">
+          <h1 className="text-2xl sm:text-3xl kg:text-4xl tracking-wide lg:tracking-wider">
+            Get Inspired
+          </h1>
+          <p className="hidden sm:block underline">Shop Insta</p>
         </div>
 
-        <div className="w-full overflow-auto px-8">
+        <div className="w-full overflow-auto px-4 lg:px-8 hide-x-scrollbar">
           <div className="flex gap-x-4">
             {numbersArray.map((num) => (
-              <div className="relative">
+              <div className="relative group cursor-pointer">
                 <div className="absolute top-4 right-4 rounded-full flex items-center justify-center">
-                  <img src="https://likeshop.me/static/img/video.svg" className="w-8" />
+                  <img
+                    src="https://likeshop.me/static/img/video.svg"
+                    className="w-8"
+                  />
                 </div>
-                <div className="w-[350px] h-[350px]">
+                <div className="w-[200px] h-[200px] sm:w-[250px] sm:h-[250px] lg:w-[350px] lg:h-[350px]">
                   <img
                     src={`/public/inspired/insta-${num}.webp`}
                     className="w-full h-full"
                     alt=""
                   />
                 </div>
-                <div className="absolute bg-[rgba(0,0,0,0.8)] inset-0 flex items-center justify-center">
-                    <p className="text-white">Shop Now</p>
+                <div className="absolute group-hover:bg-[rgba(0,0,0,0.8)] bg-[rgba(0,0,0,0)] inset-0 flex items-center justify-center transition-all duration-300 ease-linear">
+                  <p className="text-white opacity-0 group-hover:opacity-100 transition-all duration-300 ease-linear">
+                    Shop Now
+                  </p>
                 </div>
               </div>
             ))}
           </div>
         </div>
+
+        <p className="block sm:hidden text-center text-white underline">Shop Insta</p>
       </main>
     </section>
   );
