9fc26d2d4eddf8a3410b60d76d956ccc578d1562
immanuellking <emmaebubeegbosi@gmail.com>
2024-02-29 18:57:50 +0100

    changed image src for appointment section

--- src/components/Appointment.jsx
+++ src/components/Appointment.jsx
@@ -5,8 +5,16 @@ const Appointment = () => {
     <section className="px-4 lg:px-8 py-8">
       <main className="space-y-4 lg:space-y-8">
         <div className="w-full">
-          <img src="/appointments.jpg" className="w-full hidden sm:block" alt="banner" />
-          <img src="/appointment-mobile.jgp" className="w-full sm:hidden block" alt="banner" />
+          <img
+            src="https://image.brilliantearth.com/cdn-cgi/image/width=1376,height=600,quality=100,format=auto/https://cdn.builder.io/api/v1/image/assets%2F9f2a69003c86470ea05deb9ecb9887be%2F96c8bfbd49de468babfdeefa3faf6125"
+            className="w-full hidden sm:block"
+            alt="banner"
+          />
+          <img
+            src="https://image.brilliantearth.com/cdn-cgi/image/width=1472,height=1840,quality=100,format=auto/https://cdn.builder.io/api/v1/image/assets%2F9f2a69003c86470ea05deb9ecb9887be%2Ffb18ef9e5d0a480fa5f88b794e3d7163"
+            className="w-full sm:hidden block"
+            alt="banner"
+          />
         </div>
         <div className="sm:w-[60%] lg:w-[40%] space-y-4">
           <div className="space-y-2">
@@ -17,8 +25,12 @@ const Appointment = () => {
             </p>
           </div>
           <div className="flex flex-col sm:flex-row gap-2 sm:gap-4">
-            <button className="w-full border-[1px] border-black py-3 sm:py-2.5 text-sm hover:bg-[#2c2c2c] hover:text-white transition-all duration-300 ease-in-out">Visit a Showroom</button>
-            <button className="w-full border-[1px] border-black py-3 sm:py-2.5 text-sm hover:bg-[#2c2c2c] hover:text-white transition-all duration-300 ease-in-out">Book a Virtual Appointment</button>
+            <button className="w-full border-[1px] border-black py-3 sm:py-2.5 text-sm hover:bg-[#2c2c2c] hover:text-white transition-all duration-300 ease-in-out">
+              Visit a Showroom
+            </button>
+            <button className="w-full border-[1px] border-black py-3 sm:py-2.5 text-sm hover:bg-[#2c2c2c] hover:text-white transition-all duration-300 ease-in-out">
+              Book a Virtual Appointment
+            </button>
           </div>
         </div>
       </main>
