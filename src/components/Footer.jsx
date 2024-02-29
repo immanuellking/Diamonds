2c455f77d414ed140340ff8912efe79356a012a3
immanuellking <emmaebubeegbosi@gmail.com>
2024-02-29 11:11:58 +0100

    done with web view footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 import { MdKeyboardArrowRight } from "react-icons/md";
+import {
+  FaTiktok,
+  FaInstagram,
+  FaFacebookF,
+  FaPinterestP,
+  FaXTwitter,
+  FaLinkedin
+} from "react-icons/fa6";
+import { AiFillYoutube } from "react-icons/ai";
 
 const footer = [
   {
@@ -62,32 +71,63 @@ const Footer = () => {
       <main className="w-[80%] mx-auto grid grid-cols-12 gap-16">
         {footer.map((item, index) => (
           <div key={index + 1} className="col-span-4 space-y-4">
-            <h4 className="hover:text-[#0d6363] cursor-pointer hover:underline">{item.name}</h4>
+            <h4 className="hover:text-[#0d6363] cursor-pointer hover:underline uppercase">
+              {item.name}
+            </h4>
             <ul className="space-y-2">
               {item.subLinks.map((link, index) => (
-                <li key={index} className="text-[12px] hover:text-[#0d6363] cursor-pointer hover:underline">{link}</li>
+                <li
+                  key={index}
+                  className="text-[12px] hover:text-[#0d6363] cursor-pointer hover:underline"
+                >
+                  {link}
+                </li>
               ))}
             </ul>
           </div>
         ))}
         <div className="col-span-4 space-y-4">
           <div className="space-y-4">
-            <h4>SIGN UP FOR EMAIL</h4>
-            <p className="text-[12px]">Send me Brilliant Earth news, updates and offers.</p>
+            <h4 className="uppercase">SIGN UP FOR EMAIL</h4>
+            <p className="text-[12px]">
+              Send me Brilliant Earth news, updates and offers.
+            </p>
           </div>
           <div className="flex">
             <input
               type="email"
               placeholder="Your Email Address"
-              className="text-sm border-[1px] h-10 w-[250px] outline-none px-4 placeholder:text-black placeholder:text-xs"
-              name=""
+              className="text-sm border-[1px] h-10 w-[300px] outline-none px-4 placeholder:text-black placeholder:text-xs"
+              name="email"
               id=""
             />
             <div className="w-10 h10 bg-[#183E40] flex justify-center items-center">
-              <MdKeyboardArrowRight size={27} color="#fff"/>
+              <MdKeyboardArrowRight size={27} color="#fff" />
             </div>
           </div>
-          <div></div>
+          <div className="mt-8 flex gap-6 items-center">
+            <div className="w-6 h-6 bg-black rounded-md flex justify-center items-center">
+              <FaTiktok size={16} className="text-white" />
+            </div>
+            <div className=" flex justify-center items-center">
+              <AiFillYoutube size={30} className="" />
+            </div>
+            <div className=" flex justify-center items-center">
+              <FaInstagram size={28} className="" />
+            </div>
+            <div className=" flex justify-center items-center">
+              <FaFacebookF size={20} className="" />
+            </div>
+            <div className=" flex justify-center items-center">
+              <FaPinterestP size={22} className="" />
+            </div>
+            <div className=" flex justify-center items-center">
+              <FaXTwitter size={22} className="" />
+            </div>
+            <div className=" flex justify-center items-center">
+              <FaLinkedin size={25} className="" />
+            </div>
+          </div>
         </div>
       </main>
     </footer>
